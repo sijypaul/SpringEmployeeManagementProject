2c377ee7389e12466524469074ab55099666dcdc
Sijy Paul <sijypaul28@gmail.com>
2022-05-18 16:04:55 +0530

    first commit

---
+++ src/test/java/net/javaguides/springboot/springbootcurdrestfulwebservices/SpringbootCurdRestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot.springbootcurdrestfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCurdRestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
